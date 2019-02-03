84c5fbdfe358ef9ab2b2a0bbd42090aa0203a51b
opalcat <noblesse3539@daum.net>
2019-02-03 18:32:53 +0900

    Updates

---
+++ precache-manifest.2dd9049e98568283ae95a0b4acaafe00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "ff6b5a038932b9c4a7b0",
+    "url": "/movie_app/static/js/main.ff6b5a03.chunk.js"
+  },
+  {
+    "revision": "be9ff9f0d6b66aea30bf",
+    "url": "/movie_app/static/js/1.be9ff9f0.chunk.js"
+  },
+  {
+    "revision": "ff6b5a038932b9c4a7b0",
+    "url": "/movie_app/static/css/main.4cdb1584.chunk.css"
+  },
+  {
+    "revision": "181f5d00dbb7e06a6cf1d357f8d83b21",
+    "url": "/movie_app/index.html"
+  }
+];
